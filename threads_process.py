7b3ee6aa519bdbb037f7c8525d7d09de9b3317bb
unknown <rjwaji110@gmail.com>
2021-03-26 21:55:22 +0500

    version 3

---
+++ threads_process.py
@@ -0,0 +1,31 @@
+from multiprocessing import Lock, Process, Queue, current_process
+
+def main():
+    number_of_task = 10
+    number_of_processes = 4
+    tasks_to_accomplish = Queue()
+    tasks_that_are_done = Queue()
+    processes = []
+
+    for i in range(number_of_task):
+        tasks_to_accomplish.put("Task no " + str(i))
+
+    # creating processes
+    for w in range(number_of_processes):
+        p = Process(target=do_job, args=(tasks_to_accomplish, tasks_that_are_done))
+        processes.append(p)
+        p.start()
+
+    # completing process
+    for p in processes:
+        p.join()
+
+    # print the output
+    while not tasks_to_accomplish.empty():
+        print(tasks_that_are_done.get())
+
+    return True
+
+
+if __name__ == '__main__':
+    main()
